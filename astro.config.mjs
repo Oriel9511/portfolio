7abf7237b96143fa2083156bae1e9b8dea47f0bd
oriel <orielarteagaj@gmail.com>
2024-01-14 18:25:49 -0500

    Bg and cursor

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'oriel9511.github.io',
+  base: 'https://github.com/Oriel9511/portfolio.git',
 });
