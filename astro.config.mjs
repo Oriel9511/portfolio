5d6cba70a7dd5a9c10a7cb65b061ba211fb5ea24
oriel <orielarteagaj@gmail.com>
2024-10-21 21:30:10 -0400

    fixes about me

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // site: 'https://Oriel9511.github.io',
-  // base: '/',
-  build: {
-    outDir: 'dist', // Carpeta de salida
-    assets: 'public', // Carpeta de recursos públicos
-  },
-  vite: {
-    buildEnd: async () => {
-      await fs.copy('public', 'dist/public');
-    }
-  }
+  site: 'https://Oriel9511.github.io',
+  // base: '/portfolio',
 });
